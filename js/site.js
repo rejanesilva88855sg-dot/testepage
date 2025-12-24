7ecd9a11eed99257174786bda642926a26dbd056
rejanesilva88855sg-dot <rejanesilva88855sg@gmail.com>
2025-12-24 18:59:32 -0300

    js directory

--- js/site.js
+++
@@ -1,9 +0,0 @@
-fetch('config/site.json')
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById('titulo').innerText = data.titulo;
-    document.getElementById('descricao').innerText = data.descricao;
-    document.getElementById('banner').src = data.banner;
-    document.getElementById('botaoTexto').innerText = data.botao_texto;
-    document.getElementById('botao').href = data.botao_link;
-  });
